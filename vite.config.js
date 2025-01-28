9f52cb36425c7227b2c48e8ca9443668e5ab83b8
tanishktiwari <155141073+tanishktiwari@users.noreply.github.com>
2025-01-28 12:45:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['deskassure-frontend.onrender.com'],
+    allowedHosts: ['deskassure-frontend.onrender.com', 'console.deskassure.com'],
   },
 })
